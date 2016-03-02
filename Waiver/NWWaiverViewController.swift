dda84456cd0f1ec1fa084e67d0911d8fd1dd1f7f
Daniel Lee <daniel.lee@nike.com>
2016-03-02 12:35:37 -0800

    fixed some issue with the references

--- Waiver/NWWaiverViewController.swift
+++ Waiver/NWWaiverViewController.swift
@@ -8,33 +8,67 @@
 
 import UIKit
 
+public protocol NWWaiverCompletedDelegate{
+    func customerDidAccept(firstName: String, lastName: String)
+    func customerDidDecline()
+}
+
 public class NWWaiverViewController: UIViewController {
+
+    var delegate: NWWaiverCompletedDelegate! = nil
+    var waiverUrl: String! = nil
+    var locale: String! = nil
+    var userProfile: UserProfileProtocol! = nil
 
     @IBOutlet weak var waiverWebview: UIWebView!
     
     public override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.view.backgroundColor = UIColor.redColor()
         // Do any additional setup after loading the view.
+
     }
 
     public override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    //MARK: IB outlets
     
+    
+    @IBAction func declineBtnTapped(sender: AnyObject) {
+        if(delegate != nil){
+            delegate.customerDidDecline()
+        }
+    }
+
+    @IBAction func acceptBtnTapped(sender: AnyObject) {
+        if(delegate != nil){
+            delegate.customerDidAccept("First", lastName: "Last")
+        }
+    }
+
+    /**
+     * Create this controller from the bundle Xib file
+     */
     public static func fromBundleXib()->NWWaiverViewController{
-        var bundle = NSBundle(forClass: self)
-        var vc = NWWaiverViewController(nibName: "NWWaiverViewController", bundle: bundle)
-        
+        let bundle = NSBundle(forClass: self)
+        let vc = NWWaiverViewController(nibName: "NWWaiverViewController", bundle: bundle)
+
         return vc
     }
-    @IBAction func declineBtnTapped(sender: AnyObject) {
+
+    public func setWaiverUrlAndLocale(url: String, locale: String){
+        self.waiverUrl = url
+        self.locale = locale
     }
-    
-    @IBAction func acceptBtnTapped(sender: AnyObject) {
+
+    public func setUserProfile(userProfile: UserProfileProtocol){
+        self.userProfile = userProfile
     }
+
+
+
     /*
     // MARK: - Navigation
 
---
+++ Waiver/UserProfileProtocol.swift
@@ -0,0 +1,9 @@
+//
+// Created by Lee, Daniel (ETW) on 3/2/16.
+// Copyright (c) 2016 Lee, Daniel (ETW). All rights reserved.
+//
+public protocol UserProfileProtocol {
+    var firstName:String {get set}
+    var lastName:String {get set}
+    var id:Int {get set}
+}
